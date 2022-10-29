c48907446700a86532119b0a604f056e52406b5e
oluwole <olukay08@gmail.com>
2022-10-29 08:21:54 +0100

    creating the profile section

---
+++ mylittle/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <section className="App">
+      <div className="main_section">
+      <div id="profile__img">
+
+      </div>
+      </div>
+
+      
+      
+    </section>
+    
+    </>
+    );
+
+}
+
+export default App;
